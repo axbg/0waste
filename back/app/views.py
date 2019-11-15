f53b399311016680f632fcb2e3950cfd32cd4c06
Alex Bisag <bisagalexstefan@gmail.com>
2019-11-15 13:38:43 +0200

    Implemented background whitewashing

--- back/app/views.py
+++ back/app/views.py
@@ -14,10 +14,37 @@ import cv2
 import numpy as np
 
 # Create your views here.
-def get_color_by_type(type):
-    return (0, 0, 0)
+def get_color_by_type(waste_type):
+    if(waste_type == "bag"):
+        #bag - orange
+        return (0, 165, 255)
+    elif(waste_type == "bottle"):
+        #bottle - red
+        return (0, 0, 255)
+    elif(waste_type == "cap"):
+        #cap - blue
+        return (255, 0, 0)
+    elif(waste_type == "rope"):
+        #rope - green
+        return (50, 205, 50)
+    else:
+        #waste - yellow
+        return (0, 255, 255)
+            
 def get_credentials():
     return ["849576898441", "IOD3355942584437440512"]
+
+def increase_brightness(img, value=30):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    lim = 255 - value
+    v[v > lim] = 255
+    v[v <= lim] += value
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
 
 def callVision(content):
     prediction_client = automl_v1beta1.PredictionServiceClient()
@@ -34,8 +61,11 @@ def splashImage(img):
     image = Image.open(io.BytesIO(content))
     width, height = image.size
     
-    #make it gray-scale
     processed_image = np.asarray(image)[:,:,::-1].copy()
+    greyscale_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2GRAY)
+    greyscale_image = cv2.cvtColor(greyscale_image, cv2.COLOR_GRAY2RGB)
+    greyscale_image = increase_brightness(greyscale_image, 100)
+
     annotations = callVision(content)
     ct = 0
     for mark in annotations.payload:
@@ -45,9 +75,11 @@ def splashImage(img):
         end_point = (int(width * lower.x), int(height * lower.y))
         color = get_color_by_type(mark.display_name)
         thickness = 10
-        processed_image = cv2.rectangle(processed_image, start_point, end_point, color, thickness)
+        colored_cut = processed_image[int(height * upper.y):int(height * lower.y),  int(width * upper.x):int(width * lower.x)]
+        greyscale_image[int(height * upper.y):int(height * lower.y),  int(width * upper.x):int(width * lower.x)] = colored_cut
+        greyscale_image = cv2.rectangle(greyscale_image, start_point, end_point, color, thickness)
 
-    retval, buffer = cv2.imencode('.jpg', processed_image)
+    retval, buffer = cv2.imencode('.jpg', greyscale_image)
     b64img = base64.b64encode(buffer)
     return [b64img, ct]
                 
@@ -74,7 +106,26 @@ class FastTrackEndpoint(APIView):
 class ImpactMeasurementEndpoint(APIView):
     def post(self, request):
         print(request.data)
-        return Response("yas")
+        before = request.FILES.getlist('before')
+        after = request.FILES.getlist('after')
+        
+        beforeImages = []
+        afterImages = []
+        beforeObjects = []
+        afterObjects = []
+
+        for i in range(0, before.length):
+            with before[i].open("rb") as img:
+                [image, objs] = splashImage(img)
+                beforeImages.append(image)
+                beforeObjects.append(objs)
+            
+            with after[i].open("rb") as img:
+                [image, objs] = splashImage(img)
+                afterImages.append(image)
+                afterObjects.append(objs)
+
+        return Response({"beforeImages": beforeImages, "afterImages": afterImages, "beforeObjects": beforeObjects, "afterObjects": afterObjects})
 
 class CatalogueEndpoint(APIView):
     def get(self, request):
